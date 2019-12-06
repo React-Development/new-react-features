87ad7be3b6098bda539537bd8c5077d293bf4355
Brayan García <bgarcia.95@outlook.com>
2019-12-05 22:49:46 -0600

    creating counter component with hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<div>My new Content</div>, document.getElementById("root"));
+// A Hook is a function that lets you tap in to a React feature  like state or lifecycle  method.
+const App = props => {
+  const [count, setCount] = useState(props.count);
+
+  return (
+    <div>
+      <input
+        type="number"
+        placeholder="Initial Count Value"
+        value={count}
+        onChange={e => setCount(parseInt(e.target.value))}
+      />
+
+      <p>The current count is: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+};
+
+App.defaultProps = {
+  count: 0
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
