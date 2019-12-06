5ebaab20f98693a3e8efc254e9605cffc63ef009
Brayan García <bgarcia.95@outlook.com>
2019-12-06 00:56:55 -0600

    working with useEffect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
 // A Hook is a function that lets you tap in to a React feature  like state or lifecycle  method.
-
-//
 
 const NoteApp = () => {
   const [notes, setNotes] = useState([]);
@@ -21,6 +19,19 @@ const NoteApp = () => {
   const removeNote = title => {
     setNotes(notes.filter(note => note.title !== title));
   };
+
+  useEffect(()=> {
+    
+  const notesData = JSON.parse(localStorage.getItem("notes"));
+  if(notesData){
+      setNotes(notesData);
+  }
+  }, []);
+
+  useEffect(() => {
+    // When an update or didMount happens
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <div>
@@ -52,35 +63,50 @@ const NoteApp = () => {
   );
 };
 
-// const App = props => {
-//   const [count, setCount] = useState(props.count);
-//   const [text, setText] = useState("");
+const App = props => {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState("");
 
-//   return (
-//     <div>
-//       <input
-//         type="number"
-//         placeholder="Initial Count Value"
-//         value={count}
-//         onChange={e => setCount(parseInt(e.target.value))}
-//       />
 
-//       <p>
-//         The current {text || "count"} is: {count}
-//       </p>
-//       <button onClick={() => setCount(count + 1)}>+1</button>
-//       <button onClick={() => setCount(count - 1)}>-1</button>
-//       <button onClick={() => setCount(props.count)}>Reset</button>
-//           <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-//     </div>
-//   );
-// };
+  // This would be exactly as a componentDidMount
+    useEffect(() => {
+        console.log('This should only run once');
+        
+    },[]); // <- If the array is not set up, it will run anytime the text or count changes. If it's set up empty it will just run once when the component first once mounts but it won't run again because it depends on nothing. It means nothing is triggering this useEffect. Mostly used to fetch or read data.
 
-// App.defaultProps = {
-//   count: 0
-// };
+  // DidMount DidUpdate
+  // This would be exactly as a ComponentDidUpdate
+  useEffect(() => {
+    console.log("useEffect ran");
+    document.title = count;
+  }, [count]); // Here you set up specifically when the useEffect will be triggered. In this case is when count changes.
+
+  return (
+    <div>
+      <input
+        type="number"
+        placeholder="Initial Count Value"
+        value={count}
+        onChange={e => setCount(parseInt(e.target.value))}
+      />
+
+      <p>
+        The current {text || "count"} is: {count}
+      </p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
+      <input type="text" value={text} onChange={e => setText(e.target.value)} />
+    </div>
+  );
+};
+
+App.defaultProps = {
+  count: 0
+};
 
 ReactDOM.render(<NoteApp />, document.getElementById("root"));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
